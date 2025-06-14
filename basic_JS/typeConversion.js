5f7bc2d406831172c791a9973c046225eed8da85
Pu-rna <purnakundu04@gmail.com>
2025-06-14 03:42:44 +0000

    done

--- basic_JS/typeConversion.js
+++ basic_JS/typeConversion.js
@@ -48,3 +48,46 @@ let isString3 = "Purna ";//string
 let stringBoolean3 = Boolean(isString3);
 console.log(typeof stringBoolean3);//=>boolean
 console.log(stringBoolean3);//=>true
+
+
+
+//***************************  Operations **************************//
+
+let value = 3;
+let negValue = -value;
+console.log(negValue);
+
+//there are many type of operations like......
+console.log(2+2);
+console.log(2-2);
+console.log(2*3);
+console.log(2**4);
+console.log(10/5);
+console.log(12%5);
+
+//also we can add string
+let str1 = "Hello";
+let str2 = " Purna";
+
+console.log(str1+str2);//Hello Purna
+//OR
+let str3 = str1+ str2;
+console.log(str3);//Hello Purna
+
+//special case******
+//!!!!We shouldn't use this type of messy operations.
+console.log("1" + 2);//12
+console.log(1+"2");//12
+console.log("1"+2+2);//122
+console.log(1+2+"3");//33
+
+console.log(true);//op =>true
+console.log(+true);// whrn we increment the 'true' value it convert in number so op is => 1
+console.log(+" ");//=>0
+            
+                //!!!!!!!!!!!!!!!!//
+
+let num1, num2, num3;
+num1 = num2 = num3 = 2 + 2;
+
+//also we can use prefix and postfix operator
