ceac9ca6b711189b7286c5b448efe648a3c2b316
Pu-rna <purnakundu04@gmail.com>
2025-07-18 05:35:39 +0000

    done

--- 05_Iterations/06_loop.js
+++ 05_Iterations/06_loop.js
@@ -1,6 +1,6 @@
 const codding = ["js","rubby","python","java","cpp"];
 
-//orEach( (item) => {}); this process is called 'call back'
+//ForEach( (item) => {}); this process is called 'call back'
 const value = codding.forEach( (item) => {
     console.log(item);
     //to return manually the value of 'value'
@@ -32,5 +32,33 @@ nums.forEach( (roll) => {
     }
 })
 console.log(myNums);
+
+
+const books = [
+    { title: 'Book One', genre: 'Fiction', publish: 1981, edition: 2004 },
+    { title: 'Book Two', genre: 'Non-Fiction', publish: 1992, edition: 2008 },
+    { title: 'Book Three', genre: 'History', publish: 1999, edition: 2007 },
+    { title: 'Book Four', genre: 'Non-Fiction', publish: 1989, edition: 2010 },
+    { title: 'Book Five', genre: 'Science', publish: 2009, edition: 2014 },
+    { title: 'Book Six', genre: 'Fiction', publish: 1987, edition: 2010 },
+    { title: 'Book Seven', genre: 'History', publish: 1986, edition: 1996 },
+    { title: 'Book Eight', genre: 'Science', publish: 2011, edition: 2016 },
+    { title: 'Book Nine', genre: 'Non-Fiction', publish: 1981, edition: 1989 },
+  ];
+
+  const userBook = books.filter ( (bk) => bk.genre == 'History');
+  console.log(userBook);
+
+  //const userPublish = books.filter( (bk) => bk.publish >= 2000);
+  //OR
+  const userPublish = books.filter( (bk) => { return bk.publish >= 2000});
+  console.log(userPublish);
+  
+  const historyBook = books.filter((bk) => {
+    return bk.genre == 'Science' && bk.publish >= 2000
+  });
+  console.log(historyBook);
+  
+  
 
 
