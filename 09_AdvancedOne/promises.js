b2938b5fc3d6ec58a7f557382d6c6ce03f38674a
Pu-rna <purnakundu04@gmail.com>
2025-12-16 08:50:42 +0000

    done

--- 09_AdvancedOne/promises.js
+++ 09_AdvancedOne/promises.js
@@ -1,3 +1,7 @@
+//Two part:
+//i) create promise
+//ii) consume promise
+
 //create promise
 const promiseOne = new Promise(function(resolve, reject) {
     //Do an asynchronous task
@@ -26,3 +30,72 @@ new Promise(function(resolve, reject) {
 }).then(function() {
     console.log("Promise 2 consumed.");
 })
+
+
+//when a data come from network how to pass it using promises...
+//Data Consumption....................................
+const promiseThree = new Promise(function(resolve,reject) {
+    setTimeout(function() {
+        resolve({username:"Purna", email:"purnakundu04@gmail.com"})
+    },1000)
+})
+
+promiseThree.then(function(user) {
+    //whatever value pass in resolve it code in here besause .then directly connect with resolve
+    console.log(user);
+})
+
+//Uses o rejection..........................
+const promiseFour = new Promise(function(resolve,reject) {
+    setTimeout(function(){
+        let error = false;
+        if(!error)
+        {
+            resolve({username:"Megha",password:1234})
+        }else{
+            reject('ERROR: somthing went wrong!')
+        }
+    },1000)
+})
+
+promiseFour.then((user)=> {
+    console.log(user);
+    return user.username;
+}).then((myUsername) => {       //this call chaining, whatever value come from previous .then add in this .then, use in database connection.
+    console.log(myUsername);
+}).catch(function(error) {
+    console.log(error);
+}).finally(() => {
+    console.log("Promise is either resolve or rejected.");
+})  
+
+
+const promiseFive = new Promise(function(resolve,reject) {
+    setTimeout(function(){
+        let error = true;
+        if(!error)
+        {
+            resolve({username:"JavaScript",password:987});
+        }else{
+            reject("ERROR: JS went wrong!")
+        }
+    },1000)
+})
+//as a example some thime we does not want to move forward when database is not connected for some reason then we use this type of function.
+//this async await function can not handle error directly.
+//now this bellow code give error, to handle this we have to use try catch block.
+// async function consumePromiseFive() {
+//     const response = await promiseFive
+//     console.log(response);
+// }
+// consumePromiseFive();
+
+async function consumePromiseFive() {
+    try{
+        const response = await promiseFive
+        console.log(response);    
+    }catch(error){
+        console.log('ERROR!');
+    }
+}
+consumePromiseFive();
